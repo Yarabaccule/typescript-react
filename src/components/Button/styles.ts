68839b604421835bab049a79f01c2c8fb910767c
Yara <yarabaccule@yahoo.com.br>
2024-05-18 17:24:27 -0300

    Primeira vez

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: black;
+    color: #FFF;
+
+    border: 1px solid black;
+    border-radius: 2rem;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+`
